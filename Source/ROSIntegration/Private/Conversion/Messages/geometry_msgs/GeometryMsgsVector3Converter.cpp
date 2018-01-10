e8de46139682de429a90ba7c76455370c780c96e
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-01-10 16:46:50 +1000

    Add Vector3 outgoing conversion

--- Source/ROSIntegration/Private/Conversion/Messages/geometry_msgs/GeometryMsgsVector3Converter.cpp
+++ Source/ROSIntegration/Private/Conversion/Messages/geometry_msgs/GeometryMsgsVector3Converter.cpp
@@ -1,5 +1,6 @@
 #include "Conversion/Messages/geometry_msgs/GeometryMsgsVector3Converter.h"
 
+#include "geometry_msgs/Vector3.h"
 
 UGeometryMsgsVector3Converter::UGeometryMsgsVector3Converter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -12,5 +13,13 @@ bool UGeometryMsgsVector3Converter::ConvertIncomingMessage(const ROSBridgePublis
 }
 
 bool UGeometryMsgsVector3Converter::ConvertOutgoingMessage(TSharedPtr<FROSBaseMsg> BaseMsg, bson_t** message) {
+	
+	auto Vector3 = StaticCastSharedPtr<ROSMessages::geometry_msgs::Vector3>(BaseMsg);
+
+	bson_init(*message);
+	BSON_APPEND_DOUBLE(*message, "x", Vector3->x);
+	BSON_APPEND_DOUBLE(*message, "y", Vector3->x);
+	BSON_APPEND_DOUBLE(*message, "z", Vector3->x);
+	
 	return true;
 }
