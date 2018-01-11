6c87b44e6c4df58f687504d1275558169261d60b
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-01-11 14:24:15 +1000

    Add ROI converter shell code

---
+++ Source/ROSIntegration/Private/Conversion/Messages/sensor_msgs/SensorMsgsRegionOfInterestConverter.cpp
@@ -0,0 +1,20 @@
+#include "Conversion/Messages/sensor_msgs/SensorMsgsRegionOfInterestConverter.h"
+
+#include "sensor_msgs/RegionOfInterest.h"
+
+
+USensorMsgsRegionOfInterestConverter::USensorMsgsRegionOfInterestConverter(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	_MessageType = "sensor_msgs/RegionOfInterest";
+}
+
+bool USensorMsgsRegionOfInterestConverter::ConvertIncomingMessage(const ROSBridgePublishMsg* message, TSharedPtr<FROSBaseMsg> &BaseMsg) {
+	UE_LOG(LogTemp, Warning, TEXT("ROSIntegration: RegionOfInterest receiving not implemented yet"));
+	return false;
+}
+
+bool USensorMsgsRegionOfInterestConverter::ConvertOutgoingMessage(TSharedPtr<FROSBaseMsg> BaseMsg, bson_t** message) {
+	UE_LOG(LogTemp, Warning, TEXT("ROSIntegration: RegionOfInterest sending not implemented yet"));
+	return false;
+}
