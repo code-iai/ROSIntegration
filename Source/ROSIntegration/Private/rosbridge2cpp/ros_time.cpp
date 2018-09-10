5d24c62f08272b557a984deece9613baec3b0577
Richard Schubert <rs@directx9.de>
2018-09-10 18:54:38 +0200

    Added Clock Simulation ability to ROSIntegrationGameInstance

--- Source/ROSIntegration/Private/rosbridge2cpp/ros_time.cpp
+++ Source/ROSIntegration/Private/rosbridge2cpp/ros_time.cpp
@@ -1,13 +1,20 @@
 #include "ros_time.h"
 
 namespace rosbridge2cpp {
+
+	bool ROSTime::use_sim_time = false;
+	ROSTime ROSTime::sim_time = ROSTime::ROSTime(0, 0);
+
 	ROSTime ROSTime::now() {
+		if (use_sim_time) {
+			return sim_time;
+		}
+
+		const std::chrono::high_resolution_clock::duration time_since_epoch = std::chrono::high_resolution_clock::now().time_since_epoch();
 		unsigned long seconds_since_epoch =
-			std::chrono::duration_cast<std::chrono::seconds>
-			(std::chrono::system_clock::now().time_since_epoch()).count();
+			std::chrono::duration_cast<std::chrono::seconds>(time_since_epoch).count();
 		unsigned long long nanoseconds_since_epoch =
-			std::chrono::duration_cast<std::chrono::nanoseconds>
-			(std::chrono::system_clock::now().time_since_epoch()).count();
+			std::chrono::duration_cast<std::chrono::nanoseconds>(time_since_epoch).count();
 		unsigned long nanosecond_difference = nanoseconds_since_epoch - (seconds_since_epoch * 1000000000ul);
 		return ROSTime(seconds_since_epoch, nanosecond_difference);
 	}
---
+++ Source/ROSIntegration/Public/rosgraph_msgs/Clock.h
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include "ROSBaseMsg.h"
+#include "ROSTime.h"
+
+namespace ROSMessages {
+	namespace rosgraph_msgs {
+		class Clock : public FROSBaseMsg {
+		public:
+			Clock() : Clock(FROSTime()) {}
+
+			Clock(FROSTime clock) {
+				_MessageType = "rosgraph_msgs/Clock";
+				_Clock = clock;
+			}
+
+			//private:
+			FROSTime _Clock;
+		};
+	}
+}
