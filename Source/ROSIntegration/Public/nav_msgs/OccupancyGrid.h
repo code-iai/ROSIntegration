b72941746a406f8dc13521e195d5073d54bca81e
Philipp Dittmann <dittmann@uni-bremen.de>
2019-06-06 09:24:00 +0200

    Convert spaces to tabs

--- Source/ROSIntegration/Public/nav_msgs/OccupancyGrid.h
+++ Source/ROSIntegration/Public/nav_msgs/OccupancyGrid.h
@@ -6,22 +6,22 @@
 #include "nav_msgs/MapMetaData.h"
 
 namespace ROSMessages {
-    namespace nav_msgs {
-        class OccupancyGrid : public FROSBaseMsg {
-        public:
-            OccupancyGrid() {
-                _MessageType = "nav_msgs/OccupancyGrid";
-            }
-            
-            // Header header
-            std_msgs::Header header;
-            
-            // nav_msgs/MapMetaData info
-            MapMetaData info;
-            
-            // int8[] data
-            // Note: BSON will coerce the int32 to int8. int8 not implemented in BSON.
-            TArray<int32> data;
-        };
-    }
+	namespace nav_msgs {
+		class OccupancyGrid : public FROSBaseMsg {
+		public:
+			OccupancyGrid() {
+				_MessageType = "nav_msgs/OccupancyGrid";
+			}
+
+			// Header header
+			std_msgs::Header header;
+
+			// nav_msgs/MapMetaData info
+			MapMetaData info;
+
+			// int8[] data
+			// Note: BSON will coerce the int32 to int8. int8 not implemented in BSON.
+			TArray<int32> data;
+		};
+	}
 }
--- Source/ROSIntegration/Public/sensor_msgs/CompressedImage.h
+++ Source/ROSIntegration/Public/sensor_msgs/CompressedImage.h
@@ -4,29 +4,32 @@
 #include "std_msgs/Header.h"
 
 namespace ROSMessages {
-    namespace sensor_msgs {
-        class CompressedImage : public FROSBaseMsg {
-        public:
-            CompressedImage() {
-            _MessageType = "sensor_msgs/CompressedImage";
-            }
+	namespace sensor_msgs {
+		class CompressedImage : public FROSBaseMsg {
+		public:
+			CompressedImage() {
+				_MessageType = "sensor_msgs/CompressedImage";
+			}
 
-           /** Header timestamp should be acquisition time of image
-            *  Header frame_id should be optical frame of camera
-            *  origin of frame should be optical center of camera
-            *  +x should point to the right in the image
-            *  +y should point down in the image
-            *  +z should point into to plane of the image
-            */
-            ROSMessages::std_msgs::Header header;
+			/** Header timestamp should be acquisition time of image
+			 *  Header frame_id should be optical frame of camera
+			 *  origin of frame should be optical center of camera
+			 *  +x should point to the right in the image
+			 *  +y should point down in the image
+			 *  +z should point into to plane of the image
+			 */
+			ROSMessages::std_msgs::Header header;
 
-            FString format;         // Specifies the format of the data
-                                    //   Acceptable values: jpeg, png
+			/** Specifies the format of the data
+			 *  Acceptable values: jpeg, png
+			 */
+			FString format;
 
-            const uint8* data;      // Compressed image buffer
+			/** Compressed image buffer */
+			const uint8* data;
 
-            int data_size;          // image buffer size
-
-        };
-    }
+			/** image buffer size */
+			int data_size;
+		};
+	}
 }
